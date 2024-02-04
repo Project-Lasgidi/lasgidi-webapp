f9a5cb4be5e48fc0b398e8ea820e1207c9018fb7
Derek <reign.derek@yahoo.com>
2024-02-04 23:08:21 +0000

    Add more filter data

---
+++ constants/programmingLanguages.ts
@@ -0,0 +1,17 @@
+const programmingLanguages = [
+  'JavaScript',
+  'Python',
+  'Java',
+  'C#',
+  'C++',
+  'C',
+  'TypeScript',
+  'Swift',
+  'PHP',
+  'Ruby',
+  'Go',
+  'Kotlin',
+  'Rust',
+  'Scala',
+];
+export default programmingLanguages;
