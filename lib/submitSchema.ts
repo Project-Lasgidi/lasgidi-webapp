a009ef63090437f7a9a7282fff2de5eb455022c8
Derek <reign.derek@yahoo.com>
2024-04-09 23:06:03 +0000

    Add community submit schema

---
+++ lib/submitSchema.ts
@@ -0,0 +1,20 @@
+import { number, object, string, TypeOf } from 'zod';
+
+export const submitCommunitySchema = object({
+  fullName: string().min(1, 'Full name is required'),
+  email: string()
+    .min(1, { message: 'Email is required.' })
+    .email('This is not a valid email.'),
+  title: string().min(1, 'Title is required'),
+  description: string().min(1, 'Description is required'),
+  visit_url: string().min(1, 'Visit URL is required'),
+  region: string().min(1, 'Region is required'),
+  platforms: string(),
+  tool: string(),
+  language: string(),
+  logo: number(),
+});
+
+export type ISubmitCommunityRequest = TypeOf<typeof submitCommunitySchema> & {
+  publishedAt: string | null;
+};
