923648b55626304a8ed7135da63bfb2cdcb8acac
Derek <reign.derek@yahoo.com>
2024-04-09 23:10:54 +0000

    Create banner

--- actions/transformers.ts
+++ actions/transformers.ts
@@ -2,9 +2,15 @@ import { ICommunity } from '@/types';
 
 export const transformCommunity = (item: any): ICommunity => {
   const community = item.attributes;
+  const logoItem = item.attributes.logo.data.attributes;
   return {
     id: item.id,
     title: community.title,
+    logo: {
+      url: logoItem.url,
+      width: logoItem.width,
+      height: logoItem.height,
+    },
     description: community.description,
     visit_url: community.visit_url,
     is_approved: community.is_approved,
---
+++ components/Banner.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { CaretIcon } from '@/components/Icons';
+import { useRouter } from 'next/navigation';
+
+const Banner = () => {
+  const router = useRouter();
+  return (
+    <section className='flex flex-col items-center gap-6 pb-16 pt-20 md:pb-28 md:pt-24'>
+      <div className='flex flex-col items-center  gap-4'>
+        <p className='text-center text-6xl font-bold text-black'>
+          The Lasgidi Project
+        </p>
+        <p className='normal-text w-3/5 text-center md:w-full'>
+          A repository of tech communities, conferences and more.
+        </p>
+      </div>
+      <div className='grid place-items-center gap-6'>
+        <button
+          className='flex items-center gap-2 space-x-2 rounded-3xl bg-black px-14 py-3 text-base font-normal text-white md:px-6'
+          onClick={() => {
+            router.push('/submit');
+          }}
+        >
+          Submit
+          <CaretIcon />
+        </button>
+        <p className='text-base font-normal text-black'>
+          <span>Or </span>
+          <span className='underline'>Browse</span>
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Banner;
